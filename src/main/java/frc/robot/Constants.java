fb7d31a83dc922454fddd295a037d1a30d40a8e1
Ryan Yohnk <ryanyohnk@norway-2.tail34a76.ts.net>
2025-10-21 16:18:39 -0500

    Added Constants with joystick

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,10 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+
+public class Constants {
+
+    public static final XboxController joystick = new XboxController(0);
+
+}
