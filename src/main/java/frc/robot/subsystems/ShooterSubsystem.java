6c858cda718d3108cd3c3a3025152e867ecd4bab
FRC Team 5826 AvisAutomata <frc5826@gmail.com>
2025-10-21 17:05:38 -0500

    Update ShooterSubsystem.java
    Changed from a constant to variable based on the trigger

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -8,31 +8,18 @@ import static frc.robot.Constants.*;
 public class ShooterSubsystem extends SubsystemBase {
     private SparkMax leftShooterMotor;
     private SparkMax rightShooterMotor;
-    private boolean isShooting;
 
     public ShooterSubsystem() {
         leftShooterMotor = new SparkMax(leftShooterID, SparkLowLevel.MotorType.kBrushless);
         rightShooterMotor = new SparkMax(rightShooterID, SparkLowLevel.MotorType.kBrushless);
-        isShooting = false;
     }
 
-    public void shoot() {
-        isShooting = true;
-    }
-
-    public void stop() {
-        isShooting = false;
-    }
 
     @Override
     public void periodic() {
-        if (isShooting) {
-            leftShooterMotor.set(shooterSpeed);
-            rightShooterMotor.set(-shooterSpeed);
-        } else {
-            leftShooterMotor.set(0);
-            rightShooterMotor.set(0);
-        }
+        double trigger = joystick.getRightTriggerAxis();
+        leftShooterMotor.set(trigger*shooterSpeed);
+        rightShooterMotor.set(-trigger*shooterSpeed);
     }
 }
 
