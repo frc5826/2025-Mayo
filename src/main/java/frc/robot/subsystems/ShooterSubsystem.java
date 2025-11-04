0d1396a104943044b884975dba79fa7fdb514de0
FRC Team 5826 AvisAutomata <frc5826@gmail.com>
2025-11-04 16:23:53 -0600

    shooter

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -1,7 +1,10 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.spark.SparkBase;
 import com.revrobotics.spark.SparkLowLevel;
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig;
+import com.revrobotics.spark.config.SparkMaxConfig;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import static frc.robot.Constants.*;
 
@@ -16,6 +19,21 @@ public class ShooterSubsystem extends SubsystemBase {
         rightShooterMotor = new SparkMax(rightShooterID, SparkLowLevel.MotorType.kBrushless);
         leftLoaderMotor = new SparkMax(leftLoaderID, SparkLowLevel.MotorType.kBrushless);
         rightLoaderMotor = new SparkMax(rightLoaderID, SparkLowLevel.MotorType.kBrushless);
+
+        SparkMaxConfig leftConfig = new SparkMaxConfig();
+        leftConfig.inverted(false);
+        leftConfig.idleMode(SparkBaseConfig.IdleMode.kCoast);
+        leftConfig.closedLoopRampRate(0.5);
+        leftConfig.openLoopRampRate(0.5);
+        leftShooterMotor.configure(leftConfig, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters);
+
+
+        SparkMaxConfig rightConfig = new SparkMaxConfig();
+        rightConfig.follow(leftShooterMotor, true);
+        rightConfig.idleMode(SparkBaseConfig.IdleMode.kCoast);
+        rightConfig.closedLoopRampRate(0.5);
+        rightConfig.openLoopRampRate(0.5);
+        rightShooterMotor.configure(rightConfig, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters);
     }
 
 
@@ -23,8 +41,7 @@ public class ShooterSubsystem extends SubsystemBase {
     @Override
     public void periodic() {
         double trigger = joystick.getRightTriggerAxis();
-         leftShooterMotor.set(trigger*shooterSpeed);
-         rightShooterMotor.set(-trigger*shooterSpeed);
+        leftShooterMotor.set(trigger*shooterSpeed);
         boolean loader = joystick.getXButton();
         if (loader) {
             rightLoaderMotor.set(-loaderSpeed);
